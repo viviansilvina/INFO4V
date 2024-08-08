a63a45b306b433186379a639ea362d47730eec58
viviansilvina <viviansilvina23@GMAIL.COM>
2024-08-08 14:25:54 -0300

    first commit

--- francisco_maria/urls.py
+++ francisco_maria/urls.py
@@ -21,5 +21,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
-    path('francisco', TemplateView.as_view(template_name='francisco.html'))
+    path('francisco', TemplateView.as_view(template_name='francisco.html')),
+    path('maria', TemplateView.as_view(template_name='maria.html')),
+   
 ]
